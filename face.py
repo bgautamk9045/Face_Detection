cfd30d21bd8ad73253d7e2a2f5d23396882a9562
Bharti Gautam <159547430+bgautamk9045@users.noreply.github.com>
2025-07-07 15:14:37 +0530

    Update face.py

--- face.py
+++ face.py
@@ -3,19 +3,29 @@ import cv2
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('face_xml.xml')
 
-# Read the input image
-img = cv2.imread('ww.jpg')
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+while True:
+    # Read the frame
+    _, img = cap.read()
 
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-# Draw rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (50, 50, 4), 10)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (50, 50, 4), 10)
 
-# Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+# Release the VideoCapture object
+cap.release()
